b729ed260bc524f880ba9e8a3c6029969927bba3
Alejandra <alejandra.szuster@dhnn.com>
2025-11-05 17:28:07 -0300

    Footer desktop small

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,36 +6,44 @@ import Youtube from "@/svg/youtube.js";
 const Footer = () => {
   return (
     <footer className="relative bg-grey-40 h-full w-full overflow-hidden">
-      <div className="relative z-10 pt-[128px] md:pt-[64px] pb-[64px]">
-        <div className="px-[16px] md:px-[64px]">
-          <h4 className="text-grey-00 text-center">
-            La transformación <br /> no termina,{" "}
-            <span className="text-orange">evoluciona.</span>
-          </h4>
-        </div>
-        <div className="h-[300px] w-full relative mt-[32px] md:mt-[64px]">
-          <div className="absolute right-[-1px] overflow-hidden rounded-l-[999px] w-[70%] md:w-[60%] h-full gradient-border-rounded"></div>
-          <div className="absolute left-[-1px] overflow-hidden rounded-r-[999px] w-[70%] md:w-[60%] h-full gradient-border-dark"></div>
-        </div>
-        <div className="px-[16px] md:px-[64px] mt-[32px] md:mt-[64px]">
-           <Logo color="white" className="w-[92px] h-[32px] md:w-[138px] md:h-[48px]" />
-          <div className="flex items-center justify-between h-[132px] mt-[32px] md:h-auto">
-            <div className="flex flex-col gap-[16px] py-[4px] md:flex-row">
-              <button className="relative hover:pl-[20px] uppercase text-grey-20 font-medium leading-[120%] hover:text-grey-00 cursor-pointer text-left duration-300 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-0 before:h-0 before:bg-orange before:rounded-full before:duration-300 before:opacity-0 hover:before:w-[12px] hover:before:h-[12px] hover:before:opacity-100">
-                Home
-              </button>
-              <button className="relative hover:pl-[20px] uppercase text-grey-20 font-medium leading-[120%] hover:text-grey-00 cursor-pointer text-left duration-300 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-0 before:h-0 before:bg-orange before:rounded-full before:duration-300 before:opacity-0 hover:before:w-[12px] hover:before:h-[12px] hover:before:opacity-100">
-                Quienes somos
-              </button>
-              <button className="relative hover:pl-[20px] uppercase text-grey-20 font-medium leading-[120%] hover:text-grey-00 cursor-pointer text-left duration-300 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-0 before:h-0 before:bg-orange before:rounded-full before:duration-300 before:opacity-0 hover:before:w-[12px] hover:before:h-[12px] hover:before:opacity-100">
-                Casos de éxito
-              </button>
-              <button className="relative hover:pl-[20px] uppercase text-grey-20 font-medium leading-[120%] hover:text-grey-00 cursor-pointer text-left duration-300 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-0 before:h-0 before:bg-orange before:rounded-full before:duration-300 before:opacity-0 hover:before:w-[12px] hover:before:h-[12px] hover:before:opacity-100">
-                Contacto
-              </button>
+      <div className="relative z-10 pt-[128px] md:pt-[64px] pb-[64px] lg:mt-[160px]">
+        <div className="lg:relative ">
+          <div className="px-[16px] md:px-[64px] lg:absolute z-10 lg:top-[50%] lg:-translate-y-1/2">
+            <h4 className="text-grey-00 text-center lg:text-left">
+              La transformación <br /> no termina,{" "}
+              <span className="text-orange">evoluciona.</span>
+            </h4>
+          </div>
+          <div className="h-[300px] w-full relative mt-[32px] md:mt-[64px] lg:h-[598px] lg:mt-0">
+            <div className="absolute right-[-1px] overflow-hidden rounded-l-[999px] w-[70%] md:w-[60%] h-full gradient-border-rounded"></div>
+            <div className="absolute left-[-1px] overflow-hidden rounded-r-[999px] w-[70%] md:w-[60%] h-full gradient-border-dark"></div>
+          </div>
+          <div className="px-[16px] md:px-[64px] mt-[32px] md:mt-[64px] lg:absolute lg:bottom-[75px] lg:left-0 lg:flex lg:justify-between lg:items-center lg:w-full ">
+            <Logo
+              color="white"
+              className="w-[92px] h-[32px] md:w-[138px] md:h-[48px]"
+            />
+            <div className="flex items-center justify-between h-[132px] mt-[32px] md:h-auto lg:mt-0">
+              <div className="flex flex-col gap-[16px] py-[4px] md:flex-row">
+                <button className="relative hover:pl-[20px] uppercase text-grey-20 font-medium leading-[120%] hover:text-grey-00 cursor-pointer text-left duration-300 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-0 before:h-0 before:bg-orange before:rounded-full before:duration-300 before:opacity-0 hover:before:w-[12px] hover:before:h-[12px] hover:before:opacity-100">
+                  Home
+                </button>
+                <button className="relative hover:pl-[20px] uppercase text-grey-20 font-medium leading-[120%] hover:text-grey-00 cursor-pointer text-left duration-300 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-0 before:h-0 before:bg-orange before:rounded-full before:duration-300 before:opacity-0 hover:before:w-[12px] hover:before:h-[12px] hover:before:opacity-100">
+                  Quienes somos
+                </button>
+                <button className="relative hover:pl-[20px] uppercase text-grey-20 font-medium leading-[120%] hover:text-grey-00 cursor-pointer text-left duration-300 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-0 before:h-0 before:bg-orange before:rounded-full before:duration-300 before:opacity-0 hover:before:w-[12px] hover:before:h-[12px] hover:before:opacity-100">
+                  Casos de éxito
+                </button>
+                <button className="relative hover:pl-[20px] uppercase text-grey-20 font-medium leading-[120%] hover:text-grey-00 cursor-pointer text-left duration-300 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:w-0 before:h-0 before:bg-orange before:rounded-full before:duration-300 before:opacity-0 hover:before:w-[12px] hover:before:h-[12px] hover:before:opacity-100">
+                  Contacto
+                </button>
+              </div>
             </div>
           </div>
-          <hr className="w-full border-grey-20 my-[32px]" />
+        </div>
+
+        <div className="px-[16px] md:px-[64px] lg:mt-[64px]">
+          <hr className="w-full border-grey-20 my-[32px] lg:hidden" />
           <div className="flex flex-col gap-[32px] items-center md:flex-row md:justify-between">
             <div className="flex justify-between items-center w-[128px] mx-auto md:mx-0 md:order-2">
               <Instagram />
