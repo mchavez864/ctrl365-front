c429a9b38c6bd4adba68d776d4433299d6ccb9a4
Alejandra <alejandra.szuster@dhnn.com>
2025-11-05 17:30:21 -0300

    Footer desktop

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="relative bg-grey-40 h-full w-full overflow-hidden">
       <div className="relative z-10 pt-[128px] md:pt-[64px] pb-[64px] lg:mt-[160px]">
         <div className="lg:relative ">
-          <div className="px-[16px] md:px-[64px] lg:absolute z-10 lg:top-[50%] lg:-translate-y-1/2">
+          <div className="px-[16px] md:px-[64px] lg:absolute z-10 lg:top-[50%] lg:-translate-y-1/2  xxl:px-[128px]">
             <h4 className="text-grey-00 text-center lg:text-left">
               La transformación <br /> no termina,{" "}
               <span className="text-orange">evoluciona.</span>
@@ -18,7 +18,7 @@ const Footer = () => {
             <div className="absolute right-[-1px] overflow-hidden rounded-l-[999px] w-[70%] md:w-[60%] h-full gradient-border-rounded"></div>
             <div className="absolute left-[-1px] overflow-hidden rounded-r-[999px] w-[70%] md:w-[60%] h-full gradient-border-dark"></div>
           </div>
-          <div className="px-[16px] md:px-[64px] mt-[32px] md:mt-[64px] lg:absolute lg:bottom-[75px] lg:left-0 lg:flex lg:justify-between lg:items-center lg:w-full ">
+          <div className="px-[16px] md:px-[64px] mt-[32px] md:mt-[64px] lg:absolute lg:bottom-[75px] lg:left-0 lg:flex lg:justify-between lg:items-center lg:w-full xxl:px-[128px]">
             <Logo
               color="white"
               className="w-[92px] h-[32px] md:w-[138px] md:h-[48px]"
@@ -42,7 +42,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="px-[16px] md:px-[64px] lg:mt-[64px]">
+        <div className="px-[16px] md:px-[64px] lg:mt-[64px] xxl:px-[128px]">
           <hr className="w-full border-grey-20 my-[32px] lg:hidden" />
           <div className="flex flex-col gap-[32px] items-center md:flex-row md:justify-between">
             <div className="flex justify-between items-center w-[128px] mx-auto md:mx-0 md:order-2">
