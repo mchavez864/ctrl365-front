9752a3811424ffaf40a37aab8ba3b768cff48c14
Alejandra <alejandra.szuster@dhnn.com>
2025-11-18 10:46:21 -0300

    frameworks

--- src/app/[locale]/nuestro-adn/page.js
+++ src/app/[locale]/nuestro-adn/page.js
@@ -1,11 +1,13 @@
 import Navbar from '@/components/navbar/Navbar';
 import NuestroDiferencial from '@/components/nuestroADN/NuestroDiferencial';
+import Frameworks from '@/components/nuestroADN/Frameworks';
 
 export default function SuccessStoriesPage() {
   return (
     <>
       <Navbar />
       <main className="">
+        <Frameworks />
         <NuestroDiferencial />
       </main>
     </>
