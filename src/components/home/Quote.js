dc4f52ab57353e0fdf020047ac25b048111f3b9a
Matias Jaubet <matias.jaubet@dhnn.com>
2025-11-20 15:01:07 -0300

    home in esp and eng

--- src/components/home/Quote.js
+++ src/components/home/Quote.js
@@ -1,17 +1,22 @@
-"use client";
-import React, { useState, useRef, useCallback, useEffect } from "react";
+'use client';
+import React, { useState, useRef, useCallback, useEffect } from 'react';
+import { useTranslations } from 'next-intl';
 import {
   motion,
   AnimatePresence,
   useScroll,
   useMotionValueEvent,
-} from "framer-motion";
-import Image from "next/image";
-import Character from "./quote/Character";
+} from 'framer-motion';
+import Image from 'next/image';
+import Character from './quote/Character';
 
 const Quote = () => {
+  const t = useTranslations('Home.quote');
   return (
-    <section data-dark-section="true" className="relative h-[655px] md:h-[438px] lg:h-[603px] xxl:h-[651px] bg-grey-40 px-[16px] md:px-[64px] py-[64px] lg:py-[128px] xxl:px-[408px] overflow-hidden ">
+    <section
+      data-dark-section="true"
+      className="relative h-[655px] md:h-[438px] lg:h-[603px] xxl:h-[651px] bg-grey-40 px-[16px] md:px-[64px] py-[64px] lg:py-[128px] xxl:px-[408px] overflow-hidden "
+    >
       <div
         className="absolute bottom-[-81px] md:bottom-[-121px] left-1/2 -translate-x-1/2 z-10 bg-purple 
       rounded-full w-[256px] md:w-[386px] h-[179px] md:h-[269px] lg:w-[980px] lg:h-[686px]  lg:bottom-[-500px] 
@@ -21,16 +26,12 @@ const Quote = () => {
         <div className="flex items-center gap-2 ">
           <div className="bg-orange w-[12px] h-[12px] rounded-full"></div>
           <p className="text-grey-00 font-inter text-base! leading-[120%]! font-medium uppercase">
-            Menos ruido. Más impacto.
+            {t('tagline')}
           </p>
         </div>
         <p className="h3 text-grey-20">
-          <Character value="Creemos en una tecnología que genera resultados reales, no promesas. " />
-          <span className="text-grey-20 inline">
-            Diseñamos inteligencia que impulsa a las organizaciones a moverse
-            más rápido, decidir mejor y alcanzar más —con propósito real y
-            humano."
-          </span>
+          <Character value={t('text1') + ' '} />
+          <span className="text-grey-20 inline">{t('text2')}</span>
         </p>
         <div className="flex items-center gap-[16px] self-end">
           <div className="flex items-center">
@@ -45,8 +46,8 @@ const Quote = () => {
             </div>
           </div>
           <div className="flex flex-col text-grey-00 max-w-[200px] z-30 md:max-w-full">
-            <p className="font-bold!">Erik Dziubak</p>
-            <p>Chief Visionary Officer at Ctrl365</p>
+            <p className="font-bold!">{t('author.name')}</p>
+            <p>{t('author.role')}</p>
           </div>
         </div>
       </div>
