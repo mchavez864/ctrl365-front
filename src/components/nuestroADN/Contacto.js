169e79effe014860789dc84996ff5a1719a7c610
Alejandra <alejandra.szuster@dhnn.com>
2025-11-27 16:18:41 -0300

    QA Mail direction changed

--- src/components/nuestroADN/Contacto.js
+++ src/components/nuestroADN/Contacto.js
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import { useTranslations } from 'next-intl';
+import { useTranslations } from "next-intl";
 
 const Contacto = () => {
-  const t = useTranslations('NuestroAdnPage.contacto');
+  const t = useTranslations("NuestroAdnPage.contacto");
   return (
     <section
       className="relative h-[475px] md:h-[387px] lg:h-[468px] xxl:h-[468px] bg-grey-10 px-[16px] md:px-[64px] pt-[128px] pb-[64px] lg:py-[128px]  overflow-hidden lg:flex 
@@ -12,7 +12,7 @@ const Contacto = () => {
       <video
         className="absolute top-4 right-4 w-[80px] h-[80px] lg:top-1/2 lg:left-[64px] lg:-translate-y-1/2 lg:w-[200px] lg:h-[200px] xl:w-[250px] xl:h-[250px] xxl:left-[128px] object-contain pointer-events-none grayscale z-10"
         style={{
-          clipPath: 'circle(50%)',
+          clipPath: "circle(50%)",
         }}
         autoPlay
         loop
@@ -25,14 +25,14 @@ const Contacto = () => {
       </video>
       <div className="lg:w-[869px] xxl:w-[684px]">
         <h2 className="text-grey-40 pb-[8px] lg:text-[32px]! lg:max-w-[648px]">
-          {t('title')}
+          {t("title")}
         </h2>
         <p className="text-grey-40 opacity-60 mb-[64px] lg">
-          {t('description')}
+          {t("description")}
         </p>
-        <a href="mailto:rpa_recruiting@ctrl365.com">
+        <a href="mailto:recruiting@ctrl365.com">
           <p className="text-[24px]! md:text-[28px]! lg:text-[40px]! text-grey-40">
-            rpa_recruiting@ctrl365.com
+            recruiting@ctrl365.com
           </p>
         </a>
       </div>
