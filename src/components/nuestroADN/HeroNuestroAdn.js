9b7fa2a20e1634781cc7e496f8f3ee865a45222a
Matias Jaubet <matias.jaubet@dhnn.com>
2025-12-02 12:32:54 -0300

    Update HeroNuestroAdn.js

--- src/components/nuestroADN/HeroNuestroAdn.js
+++ src/components/nuestroADN/HeroNuestroAdn.js
@@ -14,13 +14,23 @@ export default async function HeroNuestroAdn() {
     >
       <div className="text-center relative z-10 py-16 lg:flex lg:items-center lg:justify-between lg:pt-64 lg:pb-32">
         <h1 className="text-grey-10 mb-8 lg:mb-0">
-          <WordReveal applyGradient centered>{t('title')}</WordReveal>
+          <WordReveal
+            applyGradient
+            centered
+          >
+            {t('title')}
+          </WordReveal>
         </h1>
         <p className="text-white !text-[18px] lg:max-w-[500px] text-left">
-          <WordReveal delay={0.3} wordGap={2}>{t('description')}</WordReveal>
+          <WordReveal
+            delay={0.3}
+            wordGap={2}
+          >
+            {t('description')}
+          </WordReveal>
         </p>
       </div>
-      <div className="relative z-20 pt-16 pb-32 lg:pt-32 lg:max-w-[824px]">
+      <div className="relative z-20 pt-16 pb-32 lg:pt-32 lg:max-w-[824px] xxl:max-w-[1226px]">
         <SmallCard
           animated
           animationDelay={0.6}
@@ -28,18 +38,40 @@ export default async function HeroNuestroAdn() {
           colorCopy="text-white"
           className="uppercase mb-4"
         />
-        <h2 className="text-white mb-8"><WordReveal delay={0.6} wordGap={5}>{t('subtitle')}</WordReveal></h2>
-        <p className="text-white !text-[18px]"><WordReveal delay={0} wordGap={2.5}>{t('content')}</WordReveal></p>
+        <h2 className="text-white mb-8">
+          <WordReveal
+            delay={0.6}
+            wordGap={5}
+          >
+            {t('subtitle')}
+          </WordReveal>
+        </h2>
+        <p className="text-white !text-[18px]">
+          <WordReveal
+            delay={0}
+            wordGap={2.5}
+          >
+            {t('content')}
+          </WordReveal>
+        </p>
       </div>
       <div className="relative z-20 flex flex-col gap-4 lg:flex-row">
-        <FadeInUp delay={0} yOffset={100} className="lg:w-[50%]">
+        <FadeInUp
+          delay={0}
+          yOffset={100}
+          className="lg:w-[50%]"
+        >
           <BigCard
             title={t('mission.title')}
             copy={t('mission.description')}
             image="/images/pages/nuestro-adn/adn-mision.webp"
           />
         </FadeInUp>
-        <FadeInUp delay={0.3} yOffset={100} className="lg:w-[50%]">
+        <FadeInUp
+          delay={0.3}
+          yOffset={100}
+          className="lg:w-[50%]"
+        >
           <BigCard
             title={t('vision.title')}
             copy={t('vision.description')}
@@ -50,7 +82,7 @@ export default async function HeroNuestroAdn() {
       <video
         className="opacity-10 absolute top-[495px] left-0 w-full z-10 overflow-hidden md:top-[295px] pointer-events-none object-cover"
         style={{
-          transform: "scale(1.3)",
+          transform: 'scale(1.3)',
         }}
         autoPlay
         loop
@@ -58,7 +90,10 @@ export default async function HeroNuestroAdn() {
         playsInline
         preload="auto"
       >
-        <source src="/videos/mesh-white.webm" type="video/webm" />
+        <source
+          src="/videos/mesh-white.webm"
+          type="video/webm"
+        />
         Your browser does not support the video tag.
       </video>
       <span className="z-0 absolute rounded-full filter blur-[150px] bottom-[-120px] left-[20%] w-[948px] h-[470px] bg-purple xxl:w-[1388px] lg:blur-[200px] xxl:h-[686px]"></span>
